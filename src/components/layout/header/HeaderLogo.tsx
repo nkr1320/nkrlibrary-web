3d22ff4685100ef9fe8bdf003a31d9cce1a51fef
nkr1320 <nakr130199@gmail.com>
2025-06-16 04:41:52 +0530

    nkrlibrary-web logo added in header

--- src/components/layout/header/HeaderLogo.tsx
+++ src/components/layout/header/HeaderLogo.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
+import logo from '../header/logonewcircle.ico';
 
 const HeaderLogo: React.FC = () => {
   return (
@@ -10,7 +11,7 @@ const HeaderLogo: React.FC = () => {
       className="flex-shrink-0"
     >
       <Link to="/" className="flex items-center text-lg sm:text-xl md:text-2xl font-bold text-primary no-underline">
-        <span>NKR LIBRARY</span>
+         <img src={logo} alt="NKR Logo" className="h-8 w-auto mr-2" /> <span>NKR LIBRARY</span>
       </Link>
     </motion.div>
   );
